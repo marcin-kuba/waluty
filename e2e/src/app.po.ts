ae657a08d23a879c7b7bf1747292c44f586acaf2
Marcin Dobroszek <marcin.dobroszek@pgf.com.pl>
2020-12-15 08:31:58 +0100

    feat(Jenkins) Add 'e2e' step
    Closed #2

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>
+    return element(by.css('app-root app-crypto-list p')).getText() as Promise<string>
   }
 }
